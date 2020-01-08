c93c0c397d61c171710b94c1be61c268b1380a3b
Rodrigo <rodrigo_ezequiel666@hotmail.com>
2020-01-08 18:07:34 -0300

    Update index.js
    additional stuff for discord bot

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -10,21 +10,18 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if (message.content === `${prefix}steampost`) {
-		message.channel.send('No Steam Post');
+		message.channel.send('https://steamcommunity.com/app/107410/discussions/10/1736634311729653971/');
 	} else if (message.content === `${prefix}modlist`) {
-		message.channel.send('No Modlist Aviable At The Moment');
+		message.channel.send('https://steamcommunity.com/sharedfiles/filedetails/?id=1954780748');
 	} else if (message.content === `${prefix}server`) {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	} else if (message.content === `${prefix}user-info`) {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);		
-	} else if (message.content === `${prefix}enjin`) {
-		message.channel.send('https://30th-ir.enjin.com/');
 	} else if (message.content === `${prefix}forums`) {
 		message.channel.send('https://30th-ir.enjin.com/');
 	} else if (message.content === `${prefix}help`) {
-		message.channel.send('"!bumpsteam" "!modlist" "!server" "!user-info" "!enjin" "!help");
-	} else if (message.content === `${prefix}teamspeak`) {
-		message..channel.send('"104.243.45.180:9004"');
+		message.channel.send('"!bumpsteam" "!modlist" "!server" "!user-info" "!enjin" "!help"' 
+			);
 	}
 });
 
