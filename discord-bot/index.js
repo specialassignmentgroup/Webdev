22fc1629fb52bbccaf456c81101cd55edce0f069
Rodrigo <rodrigo_ezequiel666@hotmail.com>
2020-01-12 01:09:57 -0300

    Update index.js

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -8,7 +8,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 
 	setInterval(() => {
-		bump.send("Daily reminder to Bump Steam recruitment post: https://steamcommunity.com/app/107410/discussions/10/1736634311729653971/");
+		bump.send("Daily reminder to Bump Steam recruitment post: ");
 	}, 2.16e+7);
 });
 
@@ -16,18 +16,19 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if (message.content === `${prefix}steampost`) {
-		message.channel.send('https://steamcommunity.com/app/107410/discussions/10/1736634311729653971/');
+		message.channel.send('NO steampost yet');
 	} else if (message.content === `${prefix}modlist`) {
-		message.channel.send('https://steamcommunity.com/sharedfiles/filedetails/?id=1954780748');
+		message.channel.send('https://steamcommunity.com/sharedfiles/filedetails/?id=1966666798');
 	} else if (message.content === `${prefix}server`) {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 	} else if (message.content === `${prefix}user-info`) {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);		
 	} else if (message.content === `${prefix}forums`) {
-		message.channel.send('https://30th-ir.enjin.com/');
+		message.channel.send('error');
 	} else if (message.content === `${prefix}help`) {
-		message.channel.send('"!steampost" "!modlist" "!server" "!user-info" "!forums" "!help"' 
-			);
+		message.channel.send('"!steampost" "!modlist" "!server" "!user-info" "!forums" "!help" "!teamspeak"');
+	} else if (message.content === `${prefix}teamspeak`) {
+		message.channel.send('64.110.27.120:10009');
 	}
 });
 // end of Basic commands 
